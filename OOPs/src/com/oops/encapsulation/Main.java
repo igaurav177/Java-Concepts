e1f94d636d7f7c8b491d1c9b5530b6fa4da68ebf
igaurav177 <gauravkumarsharma2001@gmail.com>
2023-12-10 22:39:53 +0530

    encapsulation file added

---
+++ OOPs/src/com/oops/encapsulation/Main.java
@@ -0,0 +1,10 @@
+package com.oops.encapsulation;
+
+public class Main {
+	public static void main(String[] args) {
+		Employee emp = new Employee();
+		emp.setEmpId(1);
+		emp.setEmpName("Raj");
+		System.out.println(emp);
+	}
+}
